333ed9d623d2101e13d2b765fbc8b56532e7603a
yosalx <yoseph141002@gmail.com>
2022-02-17 19:46:25 +0700

    update command

---
+++ starter-bots/java/src/main/java/za/co/entelect/challenge/command/LizardCommand.java
@@ -0,0 +1,8 @@
+package za.co.entelect.challenge.command;
+
+public class LizardCommand implements Command{
+    @Override
+    public String render() {
+        return String.format("USE_LIZARD");
+    }
+}
