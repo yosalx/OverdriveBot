7281ec4d1b02c0eb0cb6c641b5ce1eb2c022e2d8
yosalx <yoseph141002@gmail.com>
2022-02-17 19:32:34 +0700

    starter

---
+++ reference-bot/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -0,0 +1,118 @@
+package za.co.entelect.challenge;
+
+import za.co.entelect.challenge.command.*;
+import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.enums.PowerUps;
+import za.co.entelect.challenge.enums.Terrain;
+
+import java.util.*;
+
+import static java.lang.Math.max;
+
+import java.security.SecureRandom;
+
+public class Bot {
+
+    private static final int maxSpeed = 9;
+    private List<Command> directionList = new ArrayList<>();
+
+    private final Random random;
+
+    private final static Command ACCELERATE = new AccelerateCommand();
+    private final static Command LIZARD = new LizardCommand();
+    private final static Command OIL = new OilCommand();
+    private final static Command BOOST = new BoostCommand();
+    private final static Command EMP = new EmpCommand();
+    private final static Command FIX = new FixCommand();
+
+    private final static Command TURN_RIGHT = new ChangeLaneCommand(1);
+    private final static Command TURN_LEFT = new ChangeLaneCommand(-1);
+
+    public Bot() {
+        this.random = new SecureRandom();
+        directionList.add(TURN_LEFT);
+        directionList.add(TURN_RIGHT);
+    }
+
+    public Command run(GameState gameState) {
+        Car myCar = gameState.player;
+        Car opponent = gameState.opponent;
+
+        //Basic fix logic
+        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
+        List<Object> nextBlocks = blocks.subList(0,1);
+
+        //Fix first if too damaged to move
+        if(myCar.damage == 5) {
+            return FIX;
+        }
+        //Accelerate first if going to slow
+        if(myCar.speed <= 3) {
+            return ACCELERATE;
+        }
+
+        //Basic fix logic
+        if(myCar.damage >= 5) {
+            return FIX;
+        }
+
+        //Basic avoidance logic
+        if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+            if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
+                return LIZARD;
+            }
+            if (nextBlocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+                int i = random.nextInt(directionList.size());
+                return directionList.get(i);
+            }
+        }
+
+        //Basic improvement logic
+        if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
+            return BOOST;
+        }
+
+        //Basic aggression logic
+        if (myCar.speed == maxSpeed) {
+            if (hasPowerUp(PowerUps.OIL, myCar.powerups)) {
+                return OIL;
+            }
+            if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
+                return EMP;
+            }
+        }
+
+        return ACCELERATE;
+    }
+
+    private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
+        for (PowerUps powerUp: available) {
+            if (powerUp.equals(powerUpToCheck)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns map of blocks and the objects in the for the current lanes, returns
+     * the amount of blocks that can be traversed at max speed.
+     **/
+    private List<Object> getBlocksInFront(int lane, int block, GameState gameState) {
+        List<Lane[]> map = gameState.lanes;
+        List<Object> blocks = new ArrayList<>();
+        int startBlock = map.get(0)[0].position.block;
+
+        Lane[] laneList = map.get(lane - 1);
+        for (int i = max(block - startBlock, 0); i <= block - startBlock + Bot.maxSpeed; i++) {
+            if (laneList[i] == null || laneList[i].terrain == Terrain.FINISH) {
+                break;
+            }
+
+            blocks.add(laneList[i].terrain);
+
+        }
+        return blocks;
+    }
+
+}
